9a3f3faa3e32f2575b403aa7993ae46d488ffe71
Daxmatic <dax.hoes@gmail.com>
2022-11-20 01:15:05 +0100

    Initial Commit

---
+++ BPM to MS Viewer/BPM_to_MS_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BPM_to_MS_ViewerApp.swift
+//  BPM to MS Viewer
+//
+//  Created by Dax Hoes on 20/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BPM_to_MS_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
